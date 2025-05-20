7e0dcb1d8e89edd2a7e7d47c4b6de7f5abe18c33
Gregory Korsun <lvlup9119@gmail.com>
2025-05-20 15:51:48 +0300

    Returned trimStart()

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ export const App = () => {
                 className="input"
                 placeholder="Type search word"
                 value={query}
-                onChange={event => setQuery(event.target.value.trim())}
+                onChange={event => setQuery(event.target.value.trimStart())}
               />
             </div>
           </div>
