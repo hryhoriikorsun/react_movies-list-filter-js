600331b3b6de785a824c334b562a5860502ffde3
Gregory Korsun <lvlup9119@gmail.com>
2025-05-20 16:03:07 +0300

    Remove trimStart()

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ export const App = () => {
                 className="input"
                 placeholder="Type search word"
                 value={query}
-                onChange={event => setQuery(event.target.value.trimStart())}
+                onChange={event => setQuery(event.target.value)}
               />
             </div>
           </div>
